04f3f861d0cfff8f0fab7d7ef4995b0a665fbaf3
undeadtoad <110110071@qq.com>
2020-04-08 16:13:53 +0800

    aaa

--- common/consts.h
+++ common/consts.h
@@ -17,5 +17,8 @@
 #define HEAD_LENGTH 3
 #define ONE_ROOM 1
 #define MAX_FRAMEDATAS 100000
+#define BROADCAST_RATE 50
+#define BUFFER_SIZE 1024
+typedef long long ll;
 const int HALL_PORT = 17000;
 const int SERVER_PORTS[] = {17001, 17002, 17003, 17004, 17005};
