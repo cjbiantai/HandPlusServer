ba696a829ea0844082d7ee8b252a11c5f872b335
Undeadtoad <110110071@qq.com>
2020-04-20 10:13:07 -0400

    1.22 bug fixed

--- common/test.cpp
+++ common/test.cpp
@@ -6,14 +6,14 @@
 class Connection{
 	public:
         int sockfd;
+        struct sockaddr_in saddr;
 		char sendbuf[SIZE];
 		char buf[SIZE];
         Connection(){
-            struct sockaddr_in saddr;
             saddr.sin_family = AF_INET;
             saddr.sin_addr.s_addr = inet_addr("117.78.9.170");
             saddr.sin_port = htons(17000);
-            int sockfd=socket(AF_INET,SOCK_STREAM,0);
+            sockfd=socket(AF_INET,SOCK_STREAM,0);
             if(connect(sockfd,(struct sockaddr*)&saddr,sizeof(saddr))<0){
     	        printf("connect fail\n");
                 exit(0);
@@ -30,16 +30,19 @@ class Connection{
 			sendbuf[2]=(len>>8)&0xff;
 			sendbuf[1]=len&0xff;
 			cmsg.SerializeToArray(sendbuf+HEADER_LEN,len);
-			if(send(sockfd,sendbuf,len+HEADER_LEN,0)<0){
+			if(send(sockfd,sendbuf,len+HEADER_LEN,0)<=0){
 				printf("fd: %d, send fail\n",sockfd);
 				return false;
 			}
 		}
 
-		void SendRand(){
+		bool SendRand(){
 			for(int i=0;i<SIZE;i++)
 				sendbuf[i]=rand();
-			send(sockfd,sendbuf,SIZE,0);
+			if(send(sockfd,sendbuf,SIZE,0)<=0){
+				printf("fd: %d, send fail\n",sockfd);
+				return false;
+			}
 		}
 
 		bool RecvMsg(ServerMsg &smsg){
@@ -49,8 +52,8 @@ class Connection{
 			}
 			int len=(buf[4]<<24)+(buf[3]<<16)+(buf[2]<<8)+buf[1];
 			if(!smsg.ParseFromArray(buf+5,len)){
-				printf("fd: %d, parse fail\n",sockfd);
-				return false;
+				//printf("fd: %d, parse fail\n",sockfd);
+				//return false;
 			}
 			return true;
 		}
@@ -65,7 +68,6 @@ struct Params{
 
 
 void* f(void *id){
-    cout<<(ll)id<<endl;
 	Connection *conn=params[(ll)id].conn;
 	int sockfd=params[(ll)id].sockfd;
     int l=params[(ll)id].l,r=params[(ll)id].r;
@@ -80,33 +82,20 @@ void* f(void *id){
 	    playerinfo->set_account(account);
 	    conn->SendMsg(cmsg);
 	    cout<<account<<": ";
-	    if(conn->RecvMsg(smsg))
-	    	cout<<smsg.type()<<endl;
-        else
+	    if(!conn->RecvMsg(smsg))
             return NULL;
     }
     return NULL;
 }
 
-
 int main(){ 
-    /*pthread_t th[10];
+    pthread_t th[10];
     Connection conn[10];
     for(ll i=0;i<4;i++){
         params[i]=Params(2500*i,2500*(i+1),&conn[i]);
         pthread_create(&th[i],NULL,f,(void*)i);
     }
     for(int i=0;i<4;i++)
-        pthread_join(th[i],0);*/
-    Connection conn;
-    ClientMsg cmsg;
-    ServerMsg smsg;
-    cmsg.set_type(LogIn);
-	PlayerInfo *playerinfo=cmsg.mutable_playerinfo();
-    playerinfo->set_password("toad");
-    playerinfo->set_account("toad_1");
-    conn.SendMsg(cmsg);
-    conn.RecvMsg(smsg);
-    cout<<smsg.type()<<endl;
+        pthread_join(th[i],0);
     return 0;
 }
